a403fedac9089dc96a221bae49d1cf9e8bea8b44
Eshaan <eshaan.singh09@gmail.com>
2026-05-19 21:19:37 -0700

    Add additional allowed origin for CORS middleware in FastAPI

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://spelltable-beta.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
