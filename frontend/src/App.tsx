442a8b2ea0eb86656eb2d9007d3f15011ccf8d26
Eshaan <eshaan.singh09@gmail.com>
2026-05-21 12:05:04 -0700

    Update App component to manage player state and display using PlayerZone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import {useEffect, useState} from "react";
 import { fetchCard } from "./api/scryfall.ts";
-import { Zone as ZoneComponent } from "./components/CardZone/CardZone";
-import type { Zone as ZoneType } from "./types";
+import { PlayerZone } from "./components/PlayerZone/PlayerZone";
+import type { Player } from "./types";
 
 function App() {
-  const [zone, setZone] = useState<ZoneType | null>(null);
+  const [player, setPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
+    // A few sample card IDs from Scryfall. First will be used as the commander.
     const cardIds = [
-      "0e259db1-14db-4314-998c-6a076a28d8cb", // Kenrith, the Returned King
+      "0e259db1-14db-4314-998c-6a076a28d8cb", // Kenrith, the Returned King (commander)
       "4a1f905f-1d55-4d02-9d24-e58070793d3f", // Atraxa, Grand Unifier
       "63cda4a0-0dff-4edb-ae67-a2b7e2971350", // Kinnan, Bonder Prodigy
       "5a293c45-1e73-4527-be2f-2dcd5c47b610", // Sisay, Weatherlight Captain
@@ -17,19 +18,55 @@ function App() {
 
     Promise.all(cardIds.map(id => fetchCard(id)))
       .then(cards => {
-        const mockZone: ZoneType = {
-          type: "battlefield",
-          cards: cards,
-          revealed: true,
+        const [commander, ...battlefieldCards] = cards;
+
+        const mockPlayer: Player = {
+          id: "player_1",
+          name: "You",
+          life: 36,
+          commanderTax: 1,
+          zones: {
+            command: {
+              type: "command",
+              cards: [commander],
+              revealed: true,
+            },
+            battlefield: {
+              type: "battlefield",
+              cards: battlefieldCards,
+              revealed: true,
+            },
+            hand: {
+              type: "hand",
+              cards: [],
+              revealed: false,
+            },
+            graveyard: {
+              type: "graveyard",
+              cards: [],
+              revealed: true,
+            },
+            exile: {
+              type: "exile",
+              cards: [],
+              revealed: true,
+            },
+            library: {
+              type: "library",
+              cards: [],
+              revealed: false,
+            },
+          }
         };
-        setZone(mockZone);
+
+        setPlayer(mockPlayer);
       })
-      .catch(err => console.log(err));
+      .catch(err => console.error(err));
   }, []);
 
-  if (!zone) return <div>Loading...</div>;
+  if (!player) return <div>Loading...</div>;
 
-  return <ZoneComponent zone={zone} />;
+  return <PlayerZone player={player} />;
 }
 
 export default App
