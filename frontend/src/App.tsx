2ad74671268fa54c307e08b173528b50c1594588
Eshaan <eshaan.singh09@gmail.com>
2026-05-20 17:01:04 -0700

    Fetch card data and display using Card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from "react";
 import { fetchCard } from "./api/scryfall.ts";
+import { Card } from "./components/Card/Card";
+import type { Card as CardType } from "./types";
 
 function App() {
+  const [card, setCard] = useState<CardType | null>(null);
+
   useEffect(() => {
-    fetchCard('0e259db1-14db-4314-998c-6a076a28d8cb')
-        .then(card => console.log(card))
-        .catch(err => console.error(err));
+    fetchCard("0e259db1-14db-4314-998c-6a076a28d8cb")
+        .then(card => setCard(card))
+        .catch(err => console.log(err));
   }, []);
 
-  return <div>Testing Scryfall API...</div>;
+  if (!card) return <div>Loading...</div>;
+
+  return <Card card={card} />;
 }
 
 export default App
