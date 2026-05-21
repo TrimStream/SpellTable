2414e9da915a3518b87fa736aac29c615ee8eeb0
Eshaan <eshaan.singh09@gmail.com>
2026-05-20 17:14:54 -0700

    Add CardZone component and styles for displaying card zones

---
+++ frontend/src/components/CardZone/CardZone.tsx
@@ -0,0 +1,20 @@
+import type { Zone as ZoneType } from "../../types";
+import { Card } from "../Card/Card";
+import styles from './CardZone.module.css';
+
+interface ZoneProps {
+    zone: ZoneType;
+}
+
+export function Zone({ zone }: ZoneProps) {
+    return (
+        <div className={styles.container}>
+            <label>{zone.type}</label>
+            <div className={styles.cards}>
+                {zone.cards.map((card) => (
+                    <Card key={card.id} card={card} />
+                ))}
+            </div>
+        </div>
+    );
+}
